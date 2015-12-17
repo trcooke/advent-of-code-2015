35f5b087cf44c8717673832f97fde95a59d10605
Cooke, Tim(e19733) <Tim.Cooke@cmegroup.com>
2015-12-17 14:36:00 +0000

    Added EUnit tests to Day 1 solutions

--- day01-1/erlang/not_quite_lisp.erl
+++ day01-1/erlang/not_quite_lisp.erl
@@ -1,5 +1,10 @@
 -module(not_quite_lisp).
+-include_lib("eunit/include/eunit.hrl").
 -export([what_floor/1, when_basement/1]).
+
+what_floor_ground_floor_test() -> 0 = what_floor("(())").
+what_floor_second_floor_test() -> 2 = what_floor("(()(").
+what_floor_second_basement_test() -> -2 = what_floor("(())))").
 
 what_floor(Instructions) ->
     what_floor_iter(Instructions, 0).
@@ -10,6 +15,10 @@ what_floor_iter([$)|T], Floor) ->
     what_floor_iter(T, Floor - 1);
 what_floor_iter([], Floor) ->
     Floor.
+
+when_basement_never_test() -> 0 = when_basement("(())").
+when_basement_first_test() -> 1 = when_basement(")(()").
+when_basement_fifth_test() -> 5 = when_basement("(()))()").
 
 when_basement(Instructions) ->
     when_basement_iter(Instructions, 0, 0).
